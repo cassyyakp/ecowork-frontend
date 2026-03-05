5b0d411c64531548dd2463ebe8b2cb76ab180c47
johannorens <johannorens009@gmail.com>
2026-03-05 22:00:54 +0000

    ajout de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
