276ef82a47d5fade06de3882ff1eab11d8ba2ca5
Marie Cassandre <mariecassandrea2005@gmail.com>
2026-03-07 11:27:17 +0000

    modification visuel login

--- src/authentification/login.jsx
+++ src/authentification/login.jsx
@@ -51,37 +51,26 @@ export default function Login() {
     };
 
     return (
-        <div style={{ backgroundColor: '#EFF7F6', minHeight: '100vh' }}
-            className="flex items-center justify-center px-6">
+        <div className="flex items-center min-h-screen justify-center  p-6">
 
-            <div className="w-full max-w-sm">
-
-                {/* Header */}
+            <div className="w-full bg-[#EFF7F6] p-10 max-w-md rounded-xl">
                 <div className="text-center mb-10">
-                    <div style={{ backgroundColor: '#7BDFF2' }}
-                        className="w-16 h-16 rounded-full flex items-center justify-center mx-auto mb-4">
-                        <span className="text-2xl">🌿</span>
+                    <div className="w-[150px] h-[150px]  rounded-full flex items-center justify-center mx-auto mb-4">
+                        <img src="/images/logo-ecowork.png" alt="logo-ecowork" />
                     </div>
-                    <h1 className="text-2xl font-bold" style={{ color: '#3a3a3a' }}>
-                        Greenspace
-                    </h1>
-                    <p className="text-sm mt-1" style={{ color: '#888' }}>
+                    <p className="text-sm mt-1 text-[#888] ">
                         Connectez-vous à votre espace
                     </p>
                 </div>
 
-                {/* Erreur */}
                 {status === "error" && (
-                    <div style={{ backgroundColor: '#F7D6E0', color: '#c0395a' }}
-                        className="text-sm px-4 py-3 rounded-xl mb-5 text-center">
+                    <div className="text-sm px-4 py-3 bg-[#F7D6E0] text-[#c0395a] rounded-xl mb-5 text-center">
                         {errorMsg}
                     </div>
                 )}
 
-                {/* Formulaire */}
                 <div className="mb-4">
-                    <label className="block text-sm font-medium mb-1"
-                        style={{ color: '#3a3a3a' }}>
+                    <label className="block text-sm text-[#3a3a3a] font-medium mb-1">
                         Email
                     </label>
                     <input
@@ -89,15 +78,13 @@ export default function Login() {
                         name="email"
                         value={formData.email}
                         onChange={handleChange}
-                        placeholder="exemple@email.com"
-                        style={{ borderColor: '#B2F7EF', backgroundColor: '#fff' }}
-                        className="w-full border-2 rounded-xl px-4 py-3 text-sm focus:outline-none focus:border-cyan-400"
+                        placeholder="Entrez votre mail"
+                        className="w-full border-2 rounded-xl px-4 py-3 text-sm focus:outline-none focus:border-cyan-400 bg-[#fff] border-[#B2F7EF] "
                     />
                 </div>
 
                 <div className="mb-6">
-                    <label className="block text-sm font-medium mb-1"
-                        style={{ color: '#3a3a3a' }}>
+                    <label className="block text-[#3a3a3a] text-sm font-medium mb-1">
                         Mot de passe
                     </label>
                     <input
@@ -106,27 +93,24 @@ export default function Login() {
                         value={formData.password}
                         onChange={handleChange}
                         placeholder="••••••••"
-                        style={{ borderColor: '#B2F7EF', backgroundColor: '#fff' }}
-                        className="w-full border-2 rounded-xl px-4 py-3 text-sm focus:outline-none focus:border-cyan-400"
+                        className="w-full border-2 rounded-xl px-4 py-3 text-sm focus:outline-none focus:border-cyan-400 bg-[#fff] border-[#B2F7EF]"
                     />
                 </div>
 
-                {/* Bouton */}
+
                 <button
                     onClick={handleSubmit}
                     disabled={status === "loading"}
-                    style={{ backgroundColor: '#7BDFF2', color: '#3a3a3a' }}
-                    className="w-full font-semibold py-3 rounded-xl text-sm transition duration-200 active:opacity-70 disabled:opacity-50"
+                    className="w-full font-semibold py-3 rounded-xl text-sm transition duration-200 active:opacity-70 disabled:opacity-50 bg-[#7BDFF2] border-[#3a3a3a]"
                 >
                     {status === "loading" ? "Connexion..." : "Se connecter"}
                 </button>
 
-                {/* Lien register */}
-                <p className="text-center text-sm mt-6" style={{ color: '#888' }}>
+                
+                <p className="text-center text-sm mt-6 text-[#3a3a3a]">
                     Pas encore de compte ?{' '}
                     <Link to="/register"
-                        style={{ color: '#7BDFF2' }}
-                        className="font-semibold">
+                        className="font-semibold text-[#7BDFF2] ">
                         S'inscrire
                     </Link>
                 </p>
