385da4be2eef606afbf2c8b19fa57e1a7b89187c
johannorens <johannorens009@gmail.com>
2026-03-08 12:04:43 +0000

    maj app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import Layout from "./admin/layout";
 import Facture from "./admin/Facture";
 import Equipement from "./admin/Equipement";
 import Reservation from "./admin/Reservation";
+import EspaceLayout from "./admin/Espaces/EspaceLayout";
+import AjoutEspace from "./admin/Espaces/AjoutEspace";
+import EspaceShow from "./admin/Espaces/EspaceShow";
+import EspaceUpdate from "./admin/Espaces/EspaceUpadate";
 import ListeUtilisateur from "./admin/utilisateurs/ListeUtilisateur";
 import EditUtilisateur from "./admin/utilisateurs/EditUtilisateur";
 import ShowUtilisateur from "./admin/utilisateurs/ShowUtilisateur";
 import CreateAdmin from "./admin/utilisateurs/CreateAdmin";
 import "./index.css";
-import EspaceUpdate from "./admin/Espaces/EspaceUpadate";
 
 export default function App() {
   return (
@@ -23,17 +26,23 @@ export default function App() {
 
         <Route element={<Layout />}>
           <Route path="/admin/dashboard" element={<Dashboard />} />
+
+          {/* Utilisateurs */}
           <Route path="/admin/utilisateurs" element={<ListeUtilisateur />} />
           <Route path="/admin/utilisateurs/create" element={<CreateAdmin />} />
           <Route path="/admin/utilisateurs/:id" element={<ShowUtilisateur />} />
           <Route path="/admin/utilisateurs/:id/edit" element={<EditUtilisateur />} />
-          <Route path="/admin/espaces" element={<Espace />} />
+
+          {/* Espaces */}
+          <Route path="/admin/espaces" element={<EspaceLayout />} />
+          <Route path="/admin/espaces/show/:id" element={<EspaceShow />} />
+          <Route path="/admin/espaces/update/:id" element={<EspaceUpdate />} />
+          <Route path="/admin/espaces/ajout" element={<AjoutEspace />} />
+
+          {/* Autres */}
           <Route path="/admin/equipements" element={<Equipement />} />
           <Route path="/admin/reservations" element={<Reservation />} />
           <Route path="/admin/factures" element={<Facture />} />
-
-
-
         </Route>
       </Routes>
     </BrowserRouter>
