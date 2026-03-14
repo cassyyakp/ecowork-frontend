5978402056ceb4ea21ac7dc41b14a6dee023cadb
Marie Cassandre <mariecassandrea2005@gmail.com>
2026-03-14 17:56:30 +0000

    modifications mise en forme code

--- src/utilisateur/temoignage/CardTemoignage.jsx
+++ src/utilisateur/temoignage/CardTemoignage.jsx
@@ -1,8 +1,6 @@
 function CardTemoignage({ temoignage }) {
     return (
-        <div className="bg-white rounded-2xl p-6 flex flex-col gap-4"
-            style={{ borderLeft: '4px solid #F7D6E0' }}>
-
+        <div className="bg-white rounded-2xl p-6 flex flex-col gap-4 border-l-4 border-[#F7D6E0]">
 
             <div className="flex gap-1">
                 {Array.from({ length: 5 }).map((_, i) => (
@@ -12,22 +10,19 @@ function CardTemoignage({ temoignage }) {
                 ))}
             </div>
 
-            {/* Texte */}
-            <p className="text-sm italic" style={{ color: '#555' }}>
+            <p className="text-sm italic text-[#555]">
                 "{temoignage.texte}"
             </p>
 
-            {/* Auteur */}
             <div className="flex items-center gap-3 mt-2">
-                <div className="w-10 h-10 rounded-full flex items-center justify-center text-xl"
-                    style={{ backgroundColor: '#B2F7EF' }}>
+                <div className="w-10 h-10 rounded-full flex items-center justify-center text-xl bg-[#B2F7EF]">
                     {temoignage.photo}
                 </div>
                 <div>
-                    <p className="font-semibold text-sm" style={{ color: '#3a3a3a' }}>
+                    <p className="font-semibold text-[#3a3a3a] text-sm">
                         {temoignage.nom}
                     </p>
-                    <p className="text-xs" style={{ color: '#888' }}>
+                    <p className="text-xs  text-[#888]">
                         {temoignage.poste}
                     </p>
                 </div>
